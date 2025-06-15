c38555a25b2239372c186f889ecc8e2b57b2a8c8
codebynix <cnmgbeken@gmail.com>
2025-06-15 02:07:41 +0100

    Create Script.js

---
+++ Script.js
@@ -0,0 +1,36 @@
+function animator(){
+    window.location.href= "gallery.html"
+}
+function webdevelopment(){
+    window.location.href= "web.html"
+}
+function baking(){
+    window.location.href= "web.html"
+}
+function networking(){
+    window.location.href= "web.html"
+}
+function cryptocurrency(){
+    window.location.href= "web.html"
+}
+function networking(){
+    window.location.href= "web.html"
+}
+function photography(){
+    window.location.href= "web.html"
+}
+function hairdressing(){
+    window.location.href= "web.html"
+}
+function graphicdesign(){
+    window.location.href= "web.html"
+}
+function fineart(){
+    window.location.href= "web.html"
+}
+function fashiondesign(){
+    window.location.href= "web.html"
+}
+function barbing(){
+    window.location.href= "web.html"
+}
